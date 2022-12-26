04879e744e14dd2150d879a058413847d5442689
itviare <itviare@gmail.com>
2022-12-26 19:08:14 +0500

    upgraded to 2.0

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from random import randint
+import sys
 
 # print(randint(1, 50))
 name = input("Как вас зовут? : ")
-print("Tayor turing, hozir kara-kara tekshiramiz!")
+if name == "Jamshid" or name == "jamshid":
+    print("Даже не пытайся:))")
+    sys.exit()
+
+print("Мы можем помочь вам проверитьь ваши скиллы умножения от числа до чисвла!")
 n = 0
+
+d = int(input("Вводите 1 число"))
+g = int(input("Введите 2 число"))
 for i in range(10):
-    a = randint(27, 95)
-    b = randint(27, 95)
+    a = randint(d, g)
+    b = randint(d,g)
     print(f"{a} * {b} = ", end="")
     answer = int(input())
     if answer == a * b:
         n += 1
 
-print("Oyin tugadi!")
-print(f"Siz 10-tadan {n}-ta savolga togri javob bera oldiz")
+print("Игра закончилась!")
+print(f"ВЫ ответили на {n} вопросов из 10!")
