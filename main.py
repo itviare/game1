4e036c2e55cfe5fa044dd0224c85b18df8c3a5b4
itviare <itviare@gmail.com>
2022-12-26 19:05:12 +0500

    upgraded to 2.0

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from random import randint
 
 # print(randint(1, 50))
-name = input("Как вас зовут? : ")
-print("Tayor turing, hozir kara-kara tekshiramiz!")
+name = input("Как вас зовут (только реальное имя)? : ")
+print("Мы ща увидим ваши скиллы по умножению!")
 n = 0
 for i in range(10):
-    a = randint(27, 95)
-    b = randint(27, 95)
+    a = randint(20, 99)
+    b = randint(20, 99)
     print(f"{a} * {b} = ", end="")
     answer = int(input())
     if answer == a * b:
         n += 1
-
-print("Oyin tugadi!")
-print(f"Siz 10-tadan {n}-ta savolga togri javob bera oldiz")
+    elif n >= 5:
+        print('А теперь попробуй сам (без каликульятора')
+print("Game over")
+print(f"Вы ответили на {n} из 10 ")
